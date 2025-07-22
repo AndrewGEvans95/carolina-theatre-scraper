6a760603792b3caee165f8ea882e78df57607edd
Alex Kritchevsky <alex.kritchevsky@gmail.com>
2025-07-22 13:47:19 -0400

    Styling improvements
    * remove errant quote mark on more-showtimes dialog
    * highlight the row we scroll to when closing more-showtimes dialog
    * remove hardcoded styles in JS when creating copy error dialog
    * improve copy error dialog (I have no idea if copying ever errors, but I tested it by hand to make sure it worked)

--- site_generator.py
+++ site_generator.py
@@ -258,7 +258,7 @@ def generate_html(db_name="movie_showtimes.db",
             formatted_datetime = movie.get('formatted_datetime', '')
             schedule_content += (
               f"""
-              <div class='movie' id='{movie_id}' data-movie-title='{movie['title']}' data-movie-time='"{movie['time']}' data-movie-date='{date}' data-movie-cinema='{movie['cinema']}' data-formatted-datetime='{formatted_datetime}'>
+              <div class='movie' id='{movie_id}' data-movie-title='{movie['title']}' data-movie-time='{movie['time']}' data-movie-date='{date}' data-movie-cinema='{movie['cinema']}' data-formatted-datetime='{formatted_datetime}'>
                 <span class='movie-title'><a href='{movie['link']}' target='_blank'>{movie['title']}</a></span>
                 <div class='movie-info'>
                   <span class='movie-time'>{movie['time']}</span>
